e80746292bb408981d5221bec847d24cf62b8cc0
Snake0803 <carlieromain@hotmail.com>
2020-06-18 08:52:47 +0200

    Add Scriptable Object

---
+++ Scripts/CreditsData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Student", menuName = "Custom/Student", order = 0)]
+public class CreditsData : ScriptableObject
+{
+
+    public string m_studentName;
+    public string m_studentFirstName;
+    [SerializeField]
+    public enum m_studentJob
+    {
+        artist,
+        developper
+    };
+
+    public m_studentJob studentJob;
+
+    public string m_Surname;
+}
---
+++ Scripts/CreditsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsMenu : MonoBehaviour
+{
+    [SerializeField]
+    List<CreditsData> m_ListStudent = new List<CreditsData>();
+
+    [SerializeField]
+    List<Text> m_ListTextNameCanvas = new List<Text>();
+
+
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
